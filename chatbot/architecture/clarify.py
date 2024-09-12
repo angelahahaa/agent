36e357370c92379384e01a609cb4f898d4102d98
angelahahaa <angelasnpang@gmail.com>
2024-09-13 01:42:10 +0800

    clarify.py

--- chatbot/architecture/clarify.py
+++ chatbot/architecture/clarify.py
@@ -1,27 +1,34 @@
 
-from collections import defaultdict
 import logging
 import os
+from collections import defaultdict
 from datetime import datetime
-from typing import Annotated, Any, Dict, List, Literal, Set, Type, TypedDict, get_type_hints
+from functools import partial
+from typing import (Annotated, Any, Dict, List, Literal, Set, Type, TypedDict,
+                    get_type_hints)
 
+from langchain.tools.base import StructuredTool
 from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_core.messages import AIMessage, RemoveMessage, ToolMessage, BaseMessage, HumanMessage
+from langchain_core.messages import (AIMessage, BaseMessage, HumanMessage,
+                                     RemoveMessage, ToolMessage)
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import (Runnable, RunnableConfig, RunnableLambda,
+                                      RunnablePassthrough)
 from langchain_core.tools import BaseTool, tool
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, StateGraph
+from langgraph.graph import END, START, StateGraph
 from langgraph.graph.message import AnyMessage, add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from typing_extensions import TypedDict
-from langchain_core.runnables import (Runnable,
-                                      RunnableConfig, RunnableLambda, RunnablePassthrough)
+
 from chatbot.architecture.multiagent import State
+from chatbot import mocks
 from chatbot.tools import get_user_info
-from langchain.tools.base import StructuredTool
-from chatbot.mocks import mock_tool_call
+from chatbot.architecture.base import return_direct_condition
 POP = '<pop>'
+AGENT = 'agent'
+CLARIFY_AGENT = 'clarify_agent'
 
 def reducer(left:List[str], right:str|List[str]):
     if isinstance(right, str):
@@ -58,7 +65,7 @@ clarify_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-def process_pending_messages(state:ClarifyState, sensitive_tool_names:Set[str]):
+def process_pending_messages(state:ClarifyState, sensitive_tool_names:Set[str], cancel_request_tool_name:str=cancel_request.name):
     pending_message = state["pending_message"]
     updates = {
         'pending_message': None,
@@ -66,21 +73,29 @@ def process_pending_messages(state:ClarifyState, sensitive_tool_names:Set[str]):
     }
     # Create a mapping of tool names to their indices in the tool calls list
     tool_indices = defaultdict(list)
-    num_relevant_calls = 0
+    num_sensitive_calls = 0
+    has_cancel_request_tool_name = False
     
     # Filter and count relevant tool calls
     for index, tool_call in enumerate(pending_message.tool_calls):
         if tool_call['name'] in sensitive_tool_names:
             tool_indices[tool_call['name']].append(index)
-            num_relevant_calls += 1
+            num_sensitive_calls += 1
+        if tool_call['name'] == cancel_request_tool_name:
+            has_cancel_request_tool_name = True
 
     # Determine whether to keep the pending message or update it
-    if num_relevant_calls == 0:
+    if has_cancel_request_tool_name:
+        updates['pending_tool_calls'].append(POP)
+    if num_sensitive_calls == 0:
         updates['messages'] = pending_message
     else:
         # Update pending_tool_calls with relevant tool calls
         for i, name in enumerate(reversed(state['pending_tool_calls'])):
-            if i == num_relevant_calls:
+            if i==0 and has_cancel_request_tool_name:
+                # has_cancel_request_tool_name means the last tool call is already popped
+                pass
+            if i == num_sensitive_calls:
                 break
             if tool_indices.get(name):
                 tool_indices[name].pop()
@@ -104,19 +119,31 @@ def process_pending_messages(state:ClarifyState, sensitive_tool_names:Set[str]):
     
     return updates
 
+def route_process_pending_messages(state:ClarifyState):
+    if not state['messages'] or not isinstance(state['messages'][-1], AIMessage):
+        assert state['pending_tool_calls']
+        return CLARIFY_AGENT
+    return tools_condition(state)
+
+def route_tools(state:ClarifyState):
+    if state['pending_message']:
+        return CLARIFY_AGENT
+    return return_direct_condition(state)
 
 
 
 def clarify_agent_builder(
     runnable:Runnable[Any, AIMessage],
     tools:List[BaseTool], 
-    clarify_tools:List[BaseTool],
+    sensitive_tools:List[BaseTool],
     cancel_request_tool:BaseTool=cancel_request,
     state_schema:Type[Dict]=ClarifyState,
 ) -> StateGraph:
     assert  'messages' in get_type_hints(state_schema), "Key 'messages' is missing from State definition"
     assert  'pending_message' in get_type_hints(state_schema), "Key 'pending_messages' is missing from State definition"
     assert  'pending_tool_calls' in get_type_hints(state_schema), "Key 'pending_tool_calls' is missing from State definition"
+
+    sensitive_tool_names = set([t.name for t in sensitive_tools])
     
     def invoke(state, config: RunnableConfig):
         return {"pending_message":runnable.invoke(state)}
@@ -135,36 +162,37 @@ def clarify_agent_builder(
         | runnable
     )
 
-    # print(clarify_runnable.invoke({
-    #     "messages":[HumanMessage(content='hi')],
-    #     "pending_tool_calls":[AIMessage(content="",tool_calls=[mock_tool_call()])],
-    #     "user_info":"123"
-    #     }))
-    # return 
     def clarify_invoke(state, config: RunnableConfig):
         return {"pending_message":clarify_runnable.invoke(state)}
     async def clarify_ainvoke(state, config: RunnableConfig):
         return {"pending_message":await clarify_runnable.ainvoke(state, config)}
 
-def fn(state):
-    prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            " {user_info} "
-        ),
-        ("placeholder", "{messages}"),
-    ]
-    )
-    print('===')
-    # print(state)
-    print(prompt.invoke(state))
-    print('====')
-    return AIMessage(content='aha!')
-# clarify_agent_builder(RunnableLambda(fn),[],[])
+    agent = RunnableLambda(invoke, ainvoke, AGENT)
+    clarify_agent = RunnableLambda(clarify_invoke, clarify_ainvoke, CLARIFY_AGENT)
+
+    builder = StateGraph(state_schema)
+    builder.add_node(AGENT, agent)
+    builder.add_node(CLARIFY_AGENT, clarify_agent)
+    builder.add_node('process_pending_messages', partial(process_pending_messages, sensitive_tool_names=sensitive_tool_names, cancel_request_tool_name=cancel_request_tool.name))
+    builder.add_node('tools', ToolNode(tools + sensitive_tools + [cancel_request_tool]))
+    
+    builder.add_conditional_edges(START, lambda state:CLARIFY_AGENT if state['pending_tool_calls'] else AGENT, [AGENT, CLARIFY_AGENT])
+    builder.add_edge(AGENT,'process_pending_messages')
+    builder.add_edge(CLARIFY_AGENT,'process_pending_messages')
+    builder.add_conditional_edges('process_pending_messages', route_process_pending_messages, [END, "tools", CLARIFY_AGENT])
+    builder.add_conditional_edges('tools', route_tools, {END:END, 'agent':AGENT, CLARIFY_AGENT:CLARIFY_AGENT})
+    return builder
+
 
 
 if __name__ == '__main__':
+    # tests
+    tools = [mocks.mock_tool('mock_1'), mocks.mock_tool('mock_2')]
+    sensitive_tools = [mocks.mock_tool('sensitive_1'), mocks.mock_tool('sensitive_2')]
+    clarify_agent_builder(
+        mocks.MockChat().bind_tools(tools+sensitive_tools),tools,sensitive_tools
+    ).compile().get_graph(xray=False).draw_mermaid_png(output_file_path=f'graphs/{os.path.splitext(os.path.basename(__file__))[0]}.png')
+
     # tests
     def test_process_pending_messages(pm, ptcns, clarify_tool_names, expected_reduced_ptcns, expected_message):
         state = ClarifyState(
@@ -174,12 +202,14 @@ if __name__ == '__main__':
         assert result["pending_message"] is None
         actual_reduced_ptcs = reducer(ptcns, result['pending_tool_calls'])
         assert actual_reduced_ptcs == expected_reduced_ptcns, f"{actual_reduced_ptcs} != {expected_reduced_ptcns}"
-        assert (expected_message is None and result.get('messages') is None) or \
-            (sorted([tc['name'] for tc in result['messages'].tool_calls]) == sorted([tc['name'] for tc in expected_message.tool_calls]) \
+        if expected_message is None:
+            assert result.get('messages') is None, f"{result.get('messages')} != None"
+        else:
+            assert (sorted([tc['name'] for tc in result['messages'].tool_calls]) == sorted([tc['name'] for tc in expected_message.tool_calls]) \
             and result['messages'].content==expected_message.content), f"{result.get('messages')}!={expected_message}"
         
     def mock_ai_msg(context, tcs):
-        return AIMessage(content=context,tool_calls=[mock_tool_call(tc) for tc in tcs])
+        return AIMessage(content=context,tool_calls=[mocks.mock_tool_call(tc) for tc in tcs])
     test_process_pending_messages(mock_ai_msg("",[]),[], {}, [],mock_ai_msg("",[]))
     test_process_pending_messages(mock_ai_msg("meow",["1"]),[], {}, [], mock_ai_msg("meow",["1"]))
     test_process_pending_messages(mock_ai_msg("",["1"]),[], {'1'}, ['1'],None)
@@ -190,3 +220,4 @@ if __name__ == '__main__':
     test_process_pending_messages(mock_ai_msg("",["3","1","2"]) , ['1', '3'], {'1','3'}, [], mock_ai_msg("",["3","1","2"]))
     test_process_pending_messages(mock_ai_msg("",["3","1","2"]) , [], {'1','3'}, ['3','1'], mock_ai_msg("",["2"]))
     test_process_pending_messages(mock_ai_msg("meow",["3","1"]) , [], {'1','3'}, ['3','1'], None)
+    test_process_pending_messages(mock_ai_msg("meow",["3",cancel_request.name]) , ['1','2'], {'1','2','3'}, ['1','3'], mock_ai_msg("meow",cancel_request.name))
